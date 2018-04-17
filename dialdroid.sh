968577d43de9c2377a024593e2b2fcecb4b3e875
Amiangshu Bosu <amiangshu@users.noreply.github.com>
2018-04-16 21:59:26 -0500

    Removed inconsistencies between two version

--- dialdroid.sh
+++ dialdroid.sh
@@ -3,6 +3,7 @@
 dbname="dialdroid_test"
 jarfile="dialdroid.jar"
 classpath="/home/bosu/AndroidSDK/android-sdk-linux/platforms/"
+dbhost="localhost"
 
 if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters. Use: directory category"
@@ -10,7 +11,7 @@ if [ "$#" -ne 2 ]; then
 fi
 
 category=$2
-timelimit=14400
+timelimit=1800
 
 
 for file in $(find $1)
@@ -18,8 +19,9 @@ do
    if [[ $file == *.apk ]] 
    then
 	echo $file	
-	timeout $timelimit java -Xms16G -Xmx64G -jar $jarfile appanalysis $classpath $dbname $file $category
+	timeout $timelimit java -Xms16G -Xmx64G -jar $jarfile appanalysis $classpath  $dbname $dbhost $file $category
    fi
 done
 
-java -jar $jarfile computeicc $classpath $dbname 
+java -jar $jarfile computeicc $classpath $dbname $dbhost
+
